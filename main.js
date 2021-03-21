7d947ddfc66e6e8da8a0a14c313b40a84c4d12e5
beatlz <adrian@basave.com>
2021-03-21 11:18:32 -0600

    Initial github push

--- main.js
+++ main.js
@@ -37,8 +37,10 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 exports.__esModule = true;
 var credentials = require("./credentials.json");
+var users = require("./users.json");
 var Discord = require("discord.js");
 var axios_1 = require("axios");
+var fs = require("fs");
 (function () { return __awaiter(void 0, void 0, void 0, function () {
     var client, prefix_1;
     return __generator(this, function (_a) {
@@ -48,12 +50,16 @@ var axios_1 = require("axios");
                 console.log("DotaData is online!");
             });
             client.on("message", function (msg) { return __awaiter(void 0, void 0, void 0, function () {
-                var commands, content, splitCommand, params, command, doCommand, error_1;
+                var mentions_1, commands, content, splitCommand, params, command, doCommand, error_1;
                 var _a;
                 return __generator(this, function (_b) {
                     switch (_b.label) {
                         case 0:
                             _b.trys.push([0, 2, , 3]);
+                            // Setup
+                            if (!msg.content.startsWith(prefix_1) || msg.author.bot)
+                                return [2 /*return*/];
+                            mentions_1 = msg.mentions ? msg.mentions.users.map(function (user, key) { return key; }) : [];
                             commands = {
                                 ping: function () {
                                     return __awaiter(this, void 0, void 0, function () {
@@ -68,39 +74,103 @@ var axios_1 = require("axios");
                                         });
                                     });
                                 },
-                                winRate: function (limit) {
-                                    if (limit === void 0) { limit = "30"; }
+                                winrate: function (limit) {
+                                    if (limit === void 0) { limit = 30; }
                                     return __awaiter(this, void 0, void 0, function () {
-                                        var url, response, wr, error_2;
+                                        var id, tag, url, response, wr, win, lose, error_2;
                                         return __generator(this, function (_a) {
                                             switch (_a.label) {
                                                 case 0:
                                                     _a.trys.push([0, 2, , 3]);
-                                                    url = "api.opendota.com/api/players/98240008/wl?limit=" + limit;
+                                                    id = void 0;
+                                                    tag = void 0;
+                                                    if (isNaN(limit))
+                                                        limit = 30;
+                                                    if (mentions_1.length) {
+                                                        id = mentions_1.shift();
+                                                        tag = msg.mentions.users.get(id).tag;
+                                                    }
+                                                    else {
+                                                        id = msg.author.id;
+                                                        tag = msg.author.tag;
+                                                    }
+                                                    if (!users[id])
+                                                        throw new TypeError("No Dota account linked to @" + tag + ". Use dota.userConfig <steam id> to set up your account.");
+                                                    url = "http://api.opendota.com/api/players/" + users[id] + "/wl?limit=" + limit;
                                                     return [4 /*yield*/, axios_1["default"].get(url)];
                                                 case 1:
                                                     response = _a.sent();
-                                                    wr = response;
-                                                    msg.channel.send(JSON.stringify(wr));
+                                                    wr = response.data;
+                                                    win = wr.win, lose = wr.lose;
+                                                    msg.channel.send(Math.round(100 * win / (win + lose)) + "% winrate for @" + tag + " from " + win + " wins and " + lose + " losses in the last " + limit + " games.");
                                                     return [3 /*break*/, 3];
                                                 case 2:
                                                     error_2 = _a.sent();
-                                                    msg.channel.send("Error in winRate() " + error_2);
+                                                    msg.channel.send("" + error_2);
                                                     console.error("Error in winRate() " + error_2);
                                                     return [3 /*break*/, 3];
                                                 case 3: return [2 /*return*/];
                                             }
                                         });
                                     });
+                                },
+                                userConfig: function (id) {
+                                    return __awaiter(this, void 0, void 0, function () {
+                                        var authorId, url, response, playerData_1, error_3;
+                                        return __generator(this, function (_a) {
+                                            switch (_a.label) {
+                                                case 0:
+                                                    _a.trys.push([0, 3, , 4]);
+                                                    if (!id)
+                                                        throw new TypeError("Use dota.userConfig <steam id> to set up your account.");
+                                                    authorId = msg.author.id;
+                                                    users[authorId] = id;
+                                                    url = "http://api.opendota.com/api/players/" + id;
+                                                    return [4 /*yield*/, axios_1["default"].get(url)];
+                                                case 1:
+                                                    response = _a.sent();
+                                                    // @ts-ignore
+                                                    if (response.error)
+                                                        throw response.error;
+                                                    playerData_1 = response.data;
+                                                    return [4 /*yield*/, fs.writeFile("users.json", JSON.stringify(users), function (err) {
+                                                            if (err)
+                                                                throw err;
+                                                            msg.channel.send("@" + msg.author.tag + " successfully configured Steam account " + id + " to Dota player " + playerData_1.profile.personaname);
+                                                        })];
+                                                case 2:
+                                                    _a.sent();
+                                                    return [3 /*break*/, 4];
+                                                case 3:
+                                                    error_3 = _a.sent();
+                                                    msg.channel.send("Error in userConfig() " + error_3);
+                                                    console.error("Error in userConfig() " + error_3);
+                                                    return [3 /*break*/, 4];
+                                                case 4: return [2 /*return*/];
+                                            }
+                                        });
+                                    });
+                                },
+                                kills: function (limit) {
+                                    if (limit === void 0) { limit = 10; }
+                                    return __awaiter(this, void 0, void 0, function () {
+                                        return __generator(this, function (_a) {
+                                            try {
+                                            }
+                                            catch (error) {
+                                            }
+                                            return [2 /*return*/];
+                                        });
+                                    });
                                 }
                             };
-                            // Setup
-                            if (!msg.content.startsWith(prefix_1) || msg.author.bot)
-                                return [2 /*return*/];
                             content = msg.content, splitCommand = content.split(prefix_1), params = void 0, command = void 0;
                             // Command and parameters
                             params = (_a = splitCommand === null || splitCommand === void 0 ? void 0 : splitCommand.pop()) === null || _a === void 0 ? void 0 : _a.split(" ");
                             command = params.shift();
+                            // @ts-ignore
+                            if (!commands[command])
+                                throw new TypeError("Command " + command + " doesn't exist. Available commands are " + Object.keys(commands));
                             doCommand = commands[command];
                             return [4 /*yield*/, params.length];
                         case 1:
@@ -108,6 +178,7 @@ var axios_1 = require("axios");
                             return [3 /*break*/, 3];
                         case 2:
                             error_1 = _b.sent();
+                            msg.channel.send("Error: " + error_1);
                             console.error("Error in Event Emitter: " + error_1);
                             return [3 /*break*/, 3];
                         case 3: return [2 /*return*/];
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 const credentials = require("./credentials.json");
+var users = require("./users.json");
+
 import * as Discord from "discord.js";
 import axios from "axios";
+import * as fs from "fs";
 
 (async () => {
     try {
@@ -11,6 +14,10 @@ import axios from "axios";
         });
         client.on("message", async msg => {
             try {
+                // Setup
+                if(!msg.content.startsWith(prefix) || msg.author.bot) return;
+                // Mentions
+                let mentions = msg.mentions ? msg.mentions.users.map((user, key) => key) : [];
                 // Commands
                 const commands = {
                     async ping() {
@@ -20,21 +27,59 @@ import axios from "axios";
                             console.error(error);
                         }
                     },
-                    async winRate(limit="30") {
+                    async winrate(limit=30) {
                         try {
-                            let url = `api.opendota.com/api/players/98240008/wl?limit=${limit}`;
+                            let id:string;
+                            let tag:string;
+                            if(isNaN(limit)) limit = 30;
+                            if(mentions.length) {
+                                id = mentions.shift()!;
+                                tag = msg.mentions.users.get(id)!.tag;
+                            } else {
+                                id = msg.author.id;
+                                tag = msg.author.tag;
+                            }
+                            if(!users[id!])
+                            throw new TypeError(`No Dota account linked to @${tag}. Use dota.userConfig <steam id> to set up your account.`);
+                            let url = `http://api.opendota.com/api/players/${users[id]}/wl?limit=${limit}`;
                             let response = await axios.get(url);
-                            let wr = response;
+                            let wr = response.data;
+                            let { win, lose } = wr;
 
-                            msg.channel.send(JSON.stringify(wr));
+                            msg.channel.send(`${Math.round(100 * win / (win + lose))}% winrate for @${tag} from ${win} wins and ${lose} losses in the last ${limit} games.`);
                         } catch (error) {
-                            msg.channel.send(`Error in winRate() ${error}`);
+                            msg.channel.send(`${error}`);
                             console.error(`Error in winRate() ${error}`);
+                        }
+                    },
+                    async userConfig(id:string) {
+                        try {
+                            if(!id || isNaN(id?))
+                            throw new TypeError("Use dota.userConfig <steam id> to set up your account.");
+                            let authorId = msg.author.id;
+                            users[authorId] = id;
+                            let url = `http://api.opendota.com/api/players/${id}`;
+                            let response = await axios.get(url);
+                            // @ts-ignore
+                            if(response.error) throw response.error;
+                            let playerData = response.data;
+                            await fs.writeFile("users.json", JSON.stringify(users), err => {
+                                if(err) throw err;
+                                msg.channel.send(`@${msg.author.tag} successfully configured Steam account ${id} to Dota player ${playerData.profile.personaname}`);
+                            });
+                        } catch (error) {
+                            msg.channel.send(`Error in userConfig() ${error}`);
+                            console.error(`Error in userConfig() ${error}`);                            
+                        }
+                    },
+                    async kills(limit=10) {
+                        try {
+                            
+                        } catch (error) {
+                            
                         }
                     }
                 }
-                // Setup
-                if(!msg.content.startsWith(prefix) || msg.author.bot) return;
                 // Variables
                 let content:string = msg.content,
                     splitCommand:string[] = content.split(prefix),
@@ -43,9 +88,13 @@ import axios from "axios";
                 params = splitCommand?.pop()?.split(" ")!;
                 command = params.shift();
                 // @ts-ignore
+                if(!commands[command])
+                throw new TypeError(`Command ${command} doesn't exist. Available commands are ${Object.keys(commands)}`);
+                // @ts-ignore
                 let doCommand = commands[command];
                 await params.length ? doCommand(...params) : doCommand();
             } catch (error) {
+                msg.channel.send(`Error: ${error}`);
                 console.error(`Error in Event Emitter: ${error}`);
             }
         });
